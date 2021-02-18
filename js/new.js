46f326db0bc8a0e01d1c93cc59010197c2023b04
IiIymik <71165032+IiIymik@users.noreply.github.com>
2021-02-18 17:29:26 +0200

    add new function

--- js/new.js
+++ js/new.js
@@ -1,16 +1,51 @@
-console.log("helldo");
+// console.log("helldo");
 
 
-function calculateEngravingPrice(message, pricePerWord) {
-  // Пиши код ниже этой строки
+// function calculateEngravingPrice(message, pricePerWord) {
+//   // Пиши код ниже этой строки
     
-    let howManyWords = message.split(' ');
-    return console.log( howManyWords.length * pricePerWord);
+//     let howManyWords = message.split(' ');
+//     return console.log( howManyWords.length * pricePerWord);
 
-  // Пиши код выше этой строки
-}
+//   // Пиши код выше этой строки
+// }
 
 
 
-calculateEngravingPrice('JavaScript у меня в крови', 10);
-calculateEngravingPrice('JavaScript у меня в крови', 20);
+// calculateEngravingPrice('JavaScript у меня в крови', 10);
+// calculateEngravingPrice('JavaScript у меня в крови', 20);
+
+
+// function slugify(title) {
+//   // Пиши код ниже этой строки
+ 
+//   let slag = title.toLowerCase().split(' ').join('-');
+//   return console.log(slag);
+
+
+//   // Пиши код выше этой строки
+// }
+
+
+// // slugify('Массивы для новичков')
+// const fruits = ['яблоко', 'слива', 'груша', 'апельсин', 'банан'];
+// // Пиши код ниже этой строки
+// const firstTwoEls = fruits.slice(0,2);
+// const nonExtremeEls = fruits.slice(1,-1);
+// const lastThreeEls = fruits.slice(-3);
+// console.log(firstTwoEls);
+// console.log(nonExtremeEls);
+// console.log(lastThreeEls);
+
+// function makeArray(firstArray, secondArray, maxLength) {
+//     // Пиши код ниже этой строки
+// let newArray = firstArray.concat(secondArray);
+// if(newArray => maxLength){
+//   return console.log(newArray.slice(0,maxLength));
+// }
+//     return console.log(newArray);
+//     // Пиши код выше этой строки
+// }
+  
+// makeArray(['Манго', 'Поли'], ['Аякс', 'Челси'], 3);
+// makeArray(['Манго', 'Поли', 'Хьюстон'], ['Аякс', 'Челси'], 4);
