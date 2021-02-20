3161e67bf9514e6f9fbaddb61fc58fa385dfcf4d
IiIymik <71165032+IiIymik@users.noreply.github.com>
2021-02-20 11:58:01 +0200

    save

--- js/new.js
+++ js/new.js
@@ -49,3 +49,23 @@
   
 // makeArray(['Манго', 'Поли'], ['Аякс', 'Челси'], 3);
 // makeArray(['Манго', 'Поли', 'Хьюстон'], ['Аякс', 'Челси'], 4);
+
+const ex4 = [2, 65, 89];
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log("1", arr[i]);
+    for (let j = 0; j < arr.length; j++) {
+      console.log("2", arr[j]);
+      if (arr[j] > arr[j + 1]) {
+        let tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+// console.log(bubbleSort(ex4));
